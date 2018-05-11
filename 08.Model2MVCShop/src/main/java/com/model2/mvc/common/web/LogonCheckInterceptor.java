96fddc19a7620f03dcc913a5fcecd88bb7d853f7
Bit <Bit@Bitcamp>
2018-05-11 14:28:50 +0900

    UserRestController 2개 메소드 추가

--- 08.Model2MVCShop/src/main/java/com/model2/mvc/common/web/LogonCheckInterceptor.java
+++ 08.Model2MVCShop/src/main/java/com/model2/mvc/common/web/LogonCheckInterceptor.java
@@ -30,48 +30,47 @@ public class LogonCheckInterceptor extends HandlerInterceptorAdapter {
 	}
 	
 	///Method
-	public boolean preHandle(	HttpServletRequest request,
-														HttpServletResponse response, 
-														Object handler) throws Exception {
-		
-		System.out.println("\n[ LogonCheckInterceptor start........]");
-		
-		//==> 로그인 유무확인
-		HttpSession session = request.getSession(true);
-		User user = (User)session.getAttribute("user");
-
-		//==> 로그인한 회원이라면...
-		if(   user != null   )  { 
-			//==> 로그인 상태에서 접근 불가 URI
-			String uri = request.getRequestURI();
-			if(		uri.indexOf("addUserView") != -1 	|| 	uri.indexOf("addUser") != -1 || 
-					uri.indexOf("loginView") != -1 		||	uri.indexOf("login") != -1 		|| 
-					uri.indexOf("checkDuplication") != -1 ){
-				request.getRequestDispatcher("/index.jsp").forward(request, response);
-				System.out.println("[ 로그인 상태.. 로그인 후 불필요 한 요구.... ]");
-				System.out.println("[ LogonCheckInterceptor end........]\n");
-				return false;
-			}
-			System.out.println("[ 로그인 상태 ... ]");
-			System.out.println("[ LogonCheckInterceptor end........]\n");
-			return true;
-			
-		}else{ //==> 미 로그인한 화원이라면...
-			//==> 로그인 시도 중.....
-			String uri = request.getRequestURI();
-			if(		uri.indexOf("addUserView") != -1 		|| 	uri.indexOf("addUser") != -1 || 
-					uri.indexOf("loginView") != -1 			||	uri.indexOf("login") != -1 		|| 
-					uri.indexOf("checkDuplication") != -1  	||
-					uri.indexOf("listProduct")!=-1 			||	uri.indexOf("getProduct")!=-1 ){
-				System.out.println("[ 로그 시도 상태 .... ]");
-				System.out.println("[ LogonCheckInterceptor end........]\n");
-				return true;
-			}
-			request.getRequestDispatcher("/index.jsp").forward(request, response);
-			System.out.println("[ 로그인 이전 ... ]");
-			System.out.println("[ LogonCheckInterceptor end........]\n");
-			return false;
-		}
-		
-	}
+//	public boolean preHandle(	HttpServletRequest request,
+//														HttpServletResponse response, 
+//														Object handler) throws Exception {
+//		
+//		System.out.println("\n[ LogonCheckInterceptor start........]");
+//		
+//		//==> 로그인 유무확인
+//		HttpSession session = request.getSession(true);
+//		User user = (User)session.getAttribute("user");
+//
+//		//==> 로그인한 회원이라면...
+//		if(   user != null   )  { 
+//			//==> 로그인 상태에서 접근 불가 URI
+//			String uri = request.getRequestURI();
+//			if(		uri.indexOf("addUser") != -1 || uri.indexOf("login") != -1 || 
+//					uri.indexOf("checkDuplication") != -1 ){
+//				request.getRequestDispatcher("/index.jsp").forward(request, response);
+//				System.out.println("[ 로그인 상태.. 로그인 후 불필요 한 요구.... ]");
+//				System.out.println("[ LogonCheckInterceptor end........]\n");
+//				return false;
+//			}
+//			System.out.println("[ 로그인 상태 ... ]");
+//			System.out.println("[ LogonCheckInterceptor end........]\n");
+//			return true;
+//			
+//		}else{ //==> 미 로그인한 화원이라면...
+//			//==> 로그인 시도 중.....
+//			String uri = request.getRequestURI();
+//			if(		uri.indexOf("addUser") != -1 				|| uri.indexOf("login") != -1 		|| 
+//					uri.indexOf("checkDuplication") != -1  	|| uri.indexOf("listProduct")!=-1 	||
+//					uri.indexOf("getProduct")!=-1 ){
+//				System.out.println("[ 로그 시도 상태 .... ]");
+//				System.out.println("[ LogonCheckInterceptor end........]\n");
+//				return true;
+//			}
+//			request.getRequestDispatcher("/index.jsp").forward(request, response);
+//			System.out.println("[ 로그인 이전 ... ]");
+//			System.out.println("[ LogonCheckInterceptor end........]\n");
+//			return false;
+//		}
+//		
+//	}
+	
 }//end of class
---
+++ 08.Model2MVCShop/src/main/java/com/model2/mvc/web/user/UserRestController.java
@@ -0,0 +1,44 @@
+package com.model2.mvc.web.user;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.model2.mvc.service.domain.User;
+import com.model2.mvc.service.user.UserService;
+
+@RestController
+@RequestMapping("/user/*")
+public class UserRestController {
+
+	@Autowired
+	@Qualifier("userServiceImpl")
+	private UserService userService;
+	
+	public UserRestController() {
+		System.out.println(this.getClass());
+	}
+	
+	@RequestMapping(value="json/getUser/{userId}", method=RequestMethod.GET)
+	public User getUser( @PathVariable String userId ) throws Exception{
+		System.out.println("/user/json/getUser : GET");
+		
+		return userService.getUser(userId);
+	}
+
+	@RequestMapping(value="json/login", method=RequestMethod.POST)
+	public User login( @RequestBody User user, HttpSession session ) throws Exception{
+		System.out.println("/user/json/login : POST");
+		
+		System.out.println(":: "+user);
+		User dbUser = userService.getUser(user.getUserId());
+		
+		return dbUser;
+	}
+}
